c28b6fe2ae827242b1ec2a0591b0a532b106d535
Sumit Kumar <ravirajkarn@outlook.com>
2024-07-03 13:49:06 +0530

    Successfully run first CGPA calculater.

---
+++ CGPA/classs.h
@@ -0,0 +1,196 @@
+#ifndef _CLASSS_H_
+#define _CLASSS_H_
+
+#include <fmt/core.h>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <windows.h> 
+
+/**
+ * @brief class to store the data of each subject.
+ */
+class subject
+{
+private:
+    std::string subject_name;
+    int marks_scored, full_marks;
+
+public:
+    subject() = default;
+    subject(std::string name, int full, int marks_obtain);
+    std::string name() const { return subject_name; }
+    int score() const { return marks_scored; }
+    int full_mark() const { return full_marks; }
+    float point() const;
+    void init(void);
+    ~subject() = default;
+};
+
+/**
+ * @brief Construct a new subject::subject object
+ *
+ * @param name name of subject.
+ * @param full total marks of the paper.
+ * @param marks_obtain how much marks you obtain?
+ */
+subject::subject(std::string name, int full, int marks_obtain)
+{
+    this->subject_name = name;
+    this->marks_scored = marks_obtain;
+    this->full_marks = full;
+}
+
+/**
+ * @brief output point of the subject.
+ *
+ * @return char
+ */
+float subject::point() const
+{
+    return ((static_cast<float>(this->marks_scored) / this->full_marks) * 10.0f);
+}
+
+/**
+ * @brief Ask the use for subject, full marks and marks obtain.
+ */
+void subject::init()
+{
+    fmt::print("Enter subject name           : ");
+    std::cin.ignore();
+    std::getline(std::cin, subject_name);
+
+    fmt::print("Enter full marks of the paper: ");
+    std::cin >> full_marks;
+
+    fmt::print("Enter marks obtain           : ");
+    std::cin >> marks_scored;
+}
+
+/**
+ * @brief clear the console
+ */
+void clear()
+{
+    #ifdef _WIN32
+        COORD topLeft = {0, 0};
+        HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_SCREEN_BUFFER_INFO screen;
+        DWORD written;
+
+        GetConsoleScreenBufferInfo(console, &screen);
+        FillConsoleOutputCharacterA(console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+        FillConsoleOutputAttribute(console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE, screen.dwSize.X * screen.dwSize.Y, topLeft, &written);
+        SetConsoleCursorPosition(console, topLeft);
+    #else
+        std::system("clear"); // Unix (Linux and macOS)
+    #endif
+}
+
+/**
+ * @brief starting output:- header.
+ * 
+ * @param length size or length of header.
+ */
+void header(int length)
+{
+    fmt::print(fmt::fg(fmt::color::gray), "\n{0:*<{1}}\n*", "*", length);
+    fmt::print(fmt::bg(fmt::color::sky_blue) | fmt::fg(fmt::color::white_smoke) | fmt::emphasis::bold, "{0:^{1}}", "CGPA Calculator", length - 2);
+    fmt::print(fmt::fg(fmt::color::gray), "*\n");
+    fmt::print(fmt::fg(fmt::color::gray), "{0:*<{1}}\n", "*", length);
+}
+
+/**
+ * @brief personal data of students. Data Structure
+ */
+struct Child
+{
+    std::string name;  // name of the child.
+    std::string classs; // class of the child.
+    std::string school; // school of the child read.
+    std::string rollno; // rollno of the child.
+    std::string weight; // weight of the child.
+    int present; // How many days do child go to school.
+};
+
+/**
+ * @brief this will take detail of student and store in Student class.
+ *
+ * @param Student This where detail will store.
+ * @return true if successfully able to take the input.
+ * @return false if use decline to give details.
+ */
+bool give_pdetail(Child &Student)
+{
+    std::string detail = "No";
+    std::vector<std::string> correct{"True", "T", "true", "YES", "yes", "Yes", "YEs", "Y"};
+
+    fmt::print("Do you want give personal detail?(yes/no): ");
+    std::cin >> detail;
+
+    switch (detail[0])
+    {
+    case 'y':
+    case 'Y':
+    case 'T':
+    case 't':
+        for (const std::string &value : correct)
+        {
+            if ( value == detail)
+            {
+                std::cout << "Name: ";
+                std::cin.ignore();
+                std::getline(std::cin, Student.name);
+                std::cout << "Class: ";
+                std::cin >> Student.classs;
+                std::cout << "Roll No.: ";
+                std::cin >> Student.rollno;
+                std::cout << "School: ";
+                std::cin.ignore();
+                std::getline(std::cin, Student.school);
+                std::cout << "Number of present: ";
+                std::cin >> Student.present;
+                std::cout << "Weight: ";
+                std::cin >> Student.weight;
+                return true;
+            }
+        }
+        break;
+    default:
+        Student.name = "ABC";
+        Student.classs = "X";
+        Student.rollno = "1";
+        Student.school = "xyz";
+        Student.present = 220;
+        Student.weight = "85.0";
+        return false;
+        break;
+    }
+    return false;
+}
+
+/**
+ * @brief points to grade
+ *
+ * @param grade input points.
+ * @return char grade of students.
+ */
+char ptog(float grade)
+{
+    if (grade > 9)
+        return 'S';
+    else if (grade > 8)
+        return 'A';
+    else if (grade > 7)
+        return 'B';
+    else if (grade > 6)
+        return 'C';
+    else if (grade > 5.6)
+        return 'D';
+    else if (grade >= 5)
+        return 'E';
+    else
+        return 'F';
+}
+
+#endif
---
+++ CGPA/main.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file main.cpp
+ * @author Sumit Kumar (ravirajkarn@outlook.com)
+ * @brief This is simple console CGPA calculator.
+ * @version 0.1
+ * @date 2024-07-02
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <cmath>
+#include "classs.h"
+
+int main()
+{
+    int size = 94 ;
+    unsigned int no_of_subject;
+    Child Student;
+    size_t maximum = 12;
+    float total_points = 0;
+
+    //* Input module
+    header(size);
+    give_pdetail(Student); // take users personal detail
+
+    fmt::print("How many subject are there? ");
+    std::cin >> no_of_subject;
+    subject subj[no_of_subject];
+
+    for (int i = 0; i < no_of_subject; i++)
+    {
+        subj[i].init();
+        maximum = std::max(subj[i].name().size(), maximum);
+    }
+    
+
+    //* Output module
+    clear();
+    header(size);
+    fmt::print(fmt::bg(fmt::color::light_coral), "{0: <{1}}\n", " ",size);
+    fmt::print(fmt::bg(fmt::color::light_coral) | fmt::emphasis::bold, "{0: ^{1}}\n", "REPORT CARD", size);
+    fmt::print(fmt::bg(fmt::color::light_coral) | fmt::fg(fmt::color::white) | fmt::emphasis::underline, "{: <{}}\n", " ", size);
+    fmt::print(fmt::bg(fmt::color::light_coral), "{1:>10}Name: {0:<{2}}\n", Student.name, " ", size - 16);
+    fmt::print(fmt::bg(fmt::color::light_coral), "{0:>10}Class: {1:<}{0:>{2}}rollno: {3}{0:>10}\n","" , Student.classs, (size - 2 * 10 - 15 - Student.classs.size() - Student.rollno.size())  ,Student.rollno);
+    fmt::print(fmt::bg(fmt::color::light_coral), "{1:>10}School: {0:<{2}}\n", Student.school, " ", size - 18);
+    fmt::print(fmt::bg(fmt::color::light_coral), "{2:>{3}}Weight: {0}{2:>{4}}Present: {1}{2:>{3}}\n", Student.weight, Student.present, " ", 10, (size - 2 * 10 - 17 - Student.weight.size() - std::to_string(Student.present).size()));
+    fmt::print(fmt::bg(fmt::color::light_coral) | fmt::fg(fmt::color::white_smoke) | fmt::emphasis::underline, "{0:>{1}}\n", ".", size);
+
+    int x = (size -19 -47 -maximum);
+    fmt::print(fmt::bg(fmt::color::light_coral) | fmt::emphasis::underline, "| {0:>6} | {1:^{2}} | {3:^12} | {4:^14} | {5:^8} | {6:^7} |\n", "", "Subject", maximum + x, "Full Marks", "Marks Obtain", "Points", "Grade");
+    for (int i = 0; i < no_of_subject; i++)
+    {
+        fmt::print(fmt::bg(fmt::color::light_coral) | fmt::emphasis::underline, "| {0:>5}) | {1:<{2}} | {3:^12} | {4:^14} | {5:^8.2} | {6:^7} |\n", i+1, subj[i].name(), maximum + x, subj[i].full_mark(), subj[i].score(), subj[i].point(), ptog(subj[i].point()));
+        total_points =  total_points + subj[i].point();        
+    }
+    
+    fmt::print(fmt::bg(fmt::color::light_coral), "{0: <{1}}\n", " ",size);
+    fmt::print(fmt::bg(fmt::color::light_coral), "{0: >{1}}{2: <{1}}\n", "CGPA: ",size/2,(static_cast<float>(total_points/no_of_subject)));
+    fmt::print(fmt::bg(fmt::color::light_coral), "{0: <{1}}\n", " ",size);
+    return 0;
+}
